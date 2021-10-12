d1ffbf59b3afcf8cb8ba0ce496ec850fcdb16e84
Samuel Angel Rojo Lopez <71668376+samuelrojolopez@users.noreply.github.com>
2021-10-12 13:13:27 -0500

    fix: Fix extract file permissions and volumes

--- docker/spark/spark-worker/start-worker.sh
+++ docker/spark/spark-worker/start-worker.sh
@@ -8,3 +8,7 @@ mkdir -p $SPARK_WORKER_LOG_DIR
 ln -sf /dev/stdout $SPARK_WORKER_LOG_DIR/spark-worker.out
 
 $SPARK_HOME/bin/spark-class org.apache.spark.deploy.worker.Worker --webui-port $SPARK_WORKER_UI_PORT $SPARK_MASTER >> $SPARK_WORKER_LOG_DIR/spark-worker.out
+
+spark.worker.cleanup.enabled
+spark.worker.cleanup.interval
+spark.worker.cleanup.appDataTtl
